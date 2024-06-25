8bffcaff889547d8c5138db077183a9d6c63f6bb
InkerBot <im@inker.bot>
2024-06-25 15:49:01 +0800

    add github actions

--- uploader/build.gradle.kts
+++ uploader/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    application
+}
+
 dependencies {
     implementation("org.inksnow.cputil:logger:1.6")
     implementation("com.google.code.gson:gson:2.11.0")
@@ -11,4 +15,9 @@ tasks.jar {
     manifest {
         attributes["Main-Class"] = "org.inksnow.core.builder.AuroraUploader"
     }
+}
+
+tasks.getByName<JavaExec>("run") {
+    workingDir(rootProject.projectDir)
+    mainClass.set("org.inksnow.core.builder.AuroraUploader")
 }
